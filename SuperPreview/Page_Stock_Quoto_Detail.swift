98a5d9a78d33463ed2a295c751eaee0325ad83f6
PeterZ <peter.zhu@yff.com>
2023-01-18 17:20:30 +0800

    2023/01/18 无法解决，也没有研究出来为什么会触发

---
+++ SuperPreview/Page_Stock_Quoto_Detail.swift
@@ -0,0 +1,21 @@
+//
+//  Page_Stock_Quoto_Detail.swift
+//  SuperPreview
+//
+//  Created by admin on 2023/1/18.
+//  Copyright © 2023 PeterZ. All rights reserved.
+//
+
+import SwiftUI
+
+struct Page_Stock_Quoto_Detail: View {
+    var body: some View {
+        Text("Page_Stock_Quoto_Detail")
+    }
+}
+
+struct Page_Stock_Quoto_Detail_Previews: PreviewProvider {
+    static var previews: some View {
+        Page_Stock_Quoto_Detail()
+    }
+}
