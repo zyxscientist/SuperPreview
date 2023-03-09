dc52b2030df7d02bd678f8820971c4e802b0c6a6
PeterZ <peter.zhu@yff.com>
2023-03-09 17:32:44 +0800

    MarketBottomSheet起个头

---
+++ SuperPreview/Comp/MarketBottomSheet/BottomSheetView.swift
@@ -0,0 +1,21 @@
+//
+//  BottomSheetView.swift
+//  SuperPreview
+//
+//  Created by admin on 2023/3/9.
+//  Copyright © 2023 PeterZ. All rights reserved.
+//
+
+import SwiftUI
+
+struct BottomSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BottomSheetView_Previews: PreviewProvider {
+    static var previews: some View {
+        BottomSheetView()
+    }
+}
