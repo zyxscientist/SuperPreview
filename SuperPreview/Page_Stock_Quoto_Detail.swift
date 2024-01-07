ace0a9a49be2926b0188e25d0d396330554400ad
PeterZhu <zyxchelsea11@gmail.com>
2024-01-07 12:48:02 +0800

    个股标签组动画演示Demo

--- SuperPreview/Page_Stock_Quoto_Detail.swift
+++ SuperPreview/Page_Stock_Quoto_Detail.swift
@@ -9,8 +9,49 @@
 import SwiftUI
 
 struct Page_Stock_Quoto_Detail: View {
+    
+    @State var openTagGroup: Bool = false
+    @State var openMask: Bool = false
+    
     var body: some View {
-        Text("Page_Stock_Quoto_Detail")
+        VStack{
+            ZStack(alignment:.top){
+                Image("price_block")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .onTapGesture {
+                        withAnimation(.spring(response: 0.3, dampingFraction: 0.7, blendDuration: 0.8))  {
+                            openTagGroup.toggle()
+                        }
+                        openMask.toggle()
+                    }
+                
+                Rectangle()
+                    .opacity(openMask ? 0.1 : 0)
+                    .ignoresSafeArea(.all)
+                    .onTapGesture {
+                        withAnimation(.spring(response: 0.3, dampingFraction: 0.7, blendDuration: 0.8))  {
+                            openTagGroup.toggle()
+                        }
+                        openMask.toggle()
+                    }
+                
+                
+                HStack{
+                    Image("tag_group_dialog")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    }
+                    .padding(.horizontal, 15)
+                    .padding(.top, openTagGroup ? 55 : 45)
+                    .opacity(openTagGroup ? 1 : 0)
+                    .blur(radius: openTagGroup ? 0 : 2)
+                    .clipped()
+
+            }
+            
+            Spacer()
+        }
     }
 }
 
@@ -19,3 +60,5 @@ struct Page_Stock_Quoto_Detail_Previews: PreviewProvider {
         Page_Stock_Quoto_Detail()
     }
 }
+
+// .blur(radius: openGraphView ? 0 : 3)
